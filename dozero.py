bbbebc3ce3b0214e7b4ce44bd0a7dae404e2c06e
Marcus Pires <marcuspiresdecastro@gmail.com>
2025-12-04 08:35:25 -0300

    v1.7

--- dozero.py
+++ dozero.py
@@ -1,46 +1,46 @@
 from abc import ABC, abstractmethod
 
-class FilaImpressao:
+class printOne:
     _instance = None
 
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = super(FilaImpressao, cls).__new__(cls)
-            cls._instance.fila = []
+            cls._instance = super(printOne, cls).__new__(cls)
+            cls._instance.line = []
         return cls._instance
 
-    def adicionar_documento(self, documento):
-        print(f" Documento recebido na fila >> {documento[:15]}...")
-        self.fila.append(documento)
+    def add_document(self, document):
+        print(f" Documento recebido na fila >> {document[:15]}...")
+        self.line.append(document)
 
-    def imprimir_todos(self):
+    def print_all(self):
         print("\n--- INICIANDO IMPRESSÃO ---")
-        if not self.fila:
+        if not self.line:
             print("Fila vazia.")
-        for i, doc in enumerate(self.fila, 1):
-            print(f"Imprimindo Doc {i}:\n{doc}\n{'-'*30}")
-        self.fila = []
+        for test, doc in enumerate(self.line, 1):
+            print(f"Imprimindo Doc {test}:\n{doc}\n{'-'*30}")
+        self.line = []
 
 class RelatorioDraft:
     def __init__(self):
-        self.titulo = ""
-        self.corpo = []
-        self.rodape = ""
+        self.title = ""
+        self.paragraph = []
+        self.baseboard = ""
 
 class RelatorioBuilder:
     def __init__(self):
         self.draft = RelatorioDraft()
 
-    def set_titulo(self, texto):
-        self.draft.titulo = texto
+    def set_title(self, text):
+        self.draft.title = text
         return self
 
-    def add_paragrafo(self, texto):
-        self.draft.corpo.append(texto)
+    def add_paragraph(self, text):
+        self.draft.paragraph.append(text)
         return self
 
-    def set_rodape(self, texto):
-        self.draft.rodape = texto
+    def set_baseboard(self, text):
+        self.draft.baseboard = text
         return self
 
     def build(self):
@@ -53,18 +53,18 @@ class Exportador(ABC):
 
 class ExportadorHTML(Exportador):
     def exportar(self, draft):
-        html = f"<html>\n<h1>{draft.titulo}</h1>\n<body>"
-        for p in draft.corpo:
+        html = f"<html>\n<h1>{draft.title}</h1>\n<body>"
+        for p in draft.paragraph:
             html += f"\n  <p>{p}</p>"
-        html += f"\n  <footer>{draft.rodape}</footer>\n</body>\n</html>"
+        html += f"\n  <footer>{draft.baseboard}</footer>\n</body>\n</html>"
         return html
 
 class ExportadorTXT(Exportador):
     def exportar(self, draft):
-        txt = f"=== {draft.titulo.upper()} ===\n\n"
-        for p in draft.corpo:
+        txt = f"=== {draft.title.upper()} ===\n\n"
+        for p in draft.paragraph:
             txt += f"{p}\n"
-        txt += f"\n--- {draft.rodape} ---"
+        txt += f"\n--- {draft.baseboard} ---"
         return txt
 
 class ExportadorFactory:
@@ -80,23 +80,23 @@ class ExportadorFactory:
 if __name__ == "__main__":
     print("=== SISTEMA GERADOR DE RELATÓRIOS TG_CIA_LTA ===\n")
 
-    dados_relatorio = (RelatorioBuilder()
-                       .set_titulo("Relatório Mensal de Vendas")
-                       .add_paragrafo("O faturamento subiu 22% em relação a outubro.")
-                       .add_paragrafo("O produto mais vendido foi o 'Botijão P13'.")
-                       .add_paragrafo("A meta para o próximo mês é dobrar a meta.")
-                       .set_rodape("Tião do gás e cia")
+    report_data = (RelatorioBuilder()
+                       .set_title("Relatório Mensal de Vendas")
+                       .add_paragraph("O faturamento subiu 22% em relação a outubro.")
+                       .add_paragraph("O produto mais vendido foi o 'Botijão P13'.")
+                       .add_paragraph("A meta para o próximo mês é dobrar a meta.")
+                       .set_baseboard("Tião do gás e cia")
                        .build())
 
-    formato_escolhido = "txt"
+    formato_escolhido = "html"
     
     exportador = ExportadorFactory.criar_exportador(formato_escolhido)
 
-    documento_final = exportador.exportar(dados_relatorio)
+    documento_final = exportador.exportar(report_data)
 
-    spooler = FilaImpressao()
-    spooler.adicionar_documento(documento_final)
+    spooler = printOne()
+    spooler.add_document(documento_final)
 
-    spooler.adicionar_documento("Lembrete: Reunião às 09h.")
+    spooler.add_document("Lembrete: Reunião às 09h.")
 
-    spooler.imprimir_todos()
+    spooler.print_all()
---
+++ javinha.java
@@ -0,0 +1,10 @@
+public class javinha {
+    public static void main(String[] args) {
+
+        System.out.println("Meracoincidencia");
+        
+        for (int i = 0; i < args.length; i++) {
+            
+        }
+    }
+}
